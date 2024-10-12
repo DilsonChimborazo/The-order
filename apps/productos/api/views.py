7df51acd12b5572fc4542c5ac51cbb165ecfb0cf
Dilson <dilsonchimborazoperez@gmail.com>
2024-10-12 15:01:36 -0500

    router de productos

--- apps/productos/api/views.py
+++ apps/productos/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.Viewset import ModelViewSet
 from apps.productos.models import Productos
 from apps.productos.api.serializers import ProductosSerializer
 
-class ProductosSerializer(ModelViewSet):
+class ProductosModelViewSet(ModelViewSet):
     serializer_class= ProductosSerializer
     queryset = Productos.objects.all()
     http_method_names=['get', 'post', 'put', 'patch']
