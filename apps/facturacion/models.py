a3ee2ce260d515d5c61a696bff377c27a384c3b4
Dilson <dilsonchimborazoperez@gmail.com>
2024-10-17 22:30:26 -0500

    cambios en el model de facturacion

--- apps/facturacion/models.py
+++ apps/facturacion/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from apps.pedido.models import Pedido
 from apps.productos.models import Producto
+from apps.detalles.models import DetallePedido
 
 # Create your models here.
 class Facturacion(models.Model):
     pedido = models.ForeignKey(Pedido, on_delete=models.SET_NULL, null=True)
     producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, null=True)
-    cantidad = models.PositiveIntegerField()
+    cantidad_factura = models.ForeignKey(DetallePedido, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return f"{self.pedido}Total a pagar:{self.cantidad * self.producto}"
+        return f"{self.pedido}Total a pagar:{self.cantidad_factura.cantidad * self.producto.precio}"
